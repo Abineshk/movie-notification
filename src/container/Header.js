3d18edaf4900d6376685f05351ef8b3d2fa41552
Abineshk <abinesha47@gmail.com>
2020-12-05 23:17:29 +0530

    feat(header.js): Created link for react router

--- src/container/Header.js
+++ src/container/Header.js
@@ -1,5 +1,11 @@
 import React from "react";
-
+import { Link } from "react-router";
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Link to={"/login"}>Login</Link>
+      <Link to={"/signup"}>Signup</Link>
+      <Link to={"/dashboard"}>Dashboard</Link>
+    </div>
+  );
 }
