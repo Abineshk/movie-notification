3975f6900d1c01103ed709ebb326229732eee884
Abineshk <abinesha47@gmail.com>
2020-12-06 07:44:08 +0530

    feat(home): write styles for home

--- src/container/Home.js
+++ src/container/Home.js
@@ -3,15 +3,17 @@ import { movies } from "../Data/movie-data.json";
 export default function Home() {
   return (
     <div>
-      {movies.map((movie) => {
-        return (
-          <div>
-            Movie Name and Release date
-            <div>{movie.name}</div>
-            <div>{movie.releasedate}</div>
-          </div>
-        );
-      })}
+      <div className="head">Movie Name and Release date</div>
+      <div className="home">
+        {movies.map((movie) => {
+          return (
+            <div className="movie-container">
+              <div className="movie-title">{movie.name}</div>
+              <div className="movie-release-date">{movie.releasedate}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
